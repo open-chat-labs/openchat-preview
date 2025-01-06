a7cf769e6fa53367d30a8ad5802f20569aa434ba
Julian Jelfs <julian.jelfs@gmail.com>
2025-01-06 17:19:02 +0000

    report disallowed origin

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const corsOptions = {
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error(`Origin ${origin} is not permitted`));
     }
   },
 };
